60b1558be27de9eb023579156cdda0db1c07d078
zhendongliu <zhendongliu@smartsct.com>
2021-10-05 12:22:12 +0800

    jar to war

--- src/main/java/cn/edu/tju/Start.java
+++ src/main/java/cn/edu/tju/Start.java
@@ -1,5 +1,6 @@
 package cn.edu.tju;
 
+/*
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,3 +10,26 @@ public class Start {
         SpringApplication.run(Start.class,args);
     }
 }
+*/
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Start extends SpringBootServletInitializer
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(Start.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Start.class);
+    }
+}
+
+
